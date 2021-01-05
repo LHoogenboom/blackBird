98e35c096cb7e120ccafd04e193a3008efd3b882
LHoogenboom <hoogenboom.l.p@gmail.com>
2021-01-05 15:16:18 +0100

    improves timeshifting and despiking in its conceptual workings and output quality

--- main.m
+++ main.m
@@ -1,16 +1,19 @@
 %% Init
 clear; clc;
-fs = 100; % Hz
+fs = 44; % Hz
 dt = 1/fs;
 t = 0:dt:8-dt;
-u =7*uGen(t, "step",1,0);
+u =7*uGen(t, "step",1,9);
 yraw = exciteSystem(5360188,u,fs);
 
 % The peak time is at .87 seconds and the signal starts rising after a
 % delay of .45 seconds. This means that the rise time is about .42 seconds.
 % An appropriate sample rate would be to have 8 or 9 samples in this time period. 
 % So the sample time interval shoud be .42/9=.0467 seconds. i.e. a sampling 
-% frequency of 22Hz when rounded up
+% frequency of 22Hz when rounded up. In hindsight, an announcement was made
+% that the signal generation process is correllated to the sampling
+% frequency and the we were allowed to eyeball a good frequency. Double the
+% found frequency (44Hz) gives a nice workable result.
 
 % the peak of the rise after a time delay was determined to be ca. .8
 % seconds. This time was multiplied by 10 and taken as an appropriate
@@ -30,7 +33,7 @@ sysOrder = 10;
 U = genU(u); % works only for unit step input
 
 %% spikes
-y = despike(yraw,100);
+y = despike(yraw,10000,fs);
 
 
 figure(2)
@@ -39,6 +42,14 @@ plot(t,y)
 
 
 %% Timeshift
+y = timeshift(y,500,fs);
+
+cutoff = length(u)-length(y);
+t2 = t(cutoff+1:end);
+
+figure(3)
+clf; hold on;
+plot(t(cutoff+1:end),y)
 
 
 %% Functions
@@ -75,20 +86,25 @@ function U = genU(u)
     end
 end
 
-function y = despike(sig,steep)
+function y = despike(sig,slope,fs)
     for i = 2:length(sig)
-        if sig(i)-sig(i-1)>steep
+        if fs*(sig(i)-sig(i-1))>slope
             sig(i) = sig(i-1);
         end
     end
     y =sig;
 end
 
-function y = timeshift(u,y)
-    delay = .45;
-    i = 1;
-    while u(i)==0
-        i=i+i;
-    end
-    y = y(i:end)
+function y = timeshift(sig,slope,fs)
+    i = 2
+	done = logical(0)
+	while done == ~logical(1)
+        if fs*(sig(i)-sig(i-1))>slope && sig(i-1)>=0
+            sig = sig(i-1:end);
+			done = logical(1);	
+		end
+		i = i+1
+		display(i)
+	end
+    y =sig;
 end
